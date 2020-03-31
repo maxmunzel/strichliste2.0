95b8cf0cb48487e66a99709e452a369defb7c494
Max Nagy <maxmunzel@me.com>
2020-03-31 22:46:38 +0200

    Added rudimentary Product View

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,4 +1,4 @@
-module Grid exposing (Model(..), Msg(..), User, getUsers, init, main, subscriptions, update, userDecoder, userListDecoder, userView, view)
+module Grid exposing (Model(..), Msg(..), User, getUsers, init, main, subscriptions, update, userDecoder, userView, view)
 
 import Browser
 import Design
@@ -45,7 +45,8 @@ type Model
     = Failure
     | Loading
     | Loaded (List User)
-    | ProductView User
+    | LoadingProducts User
+    | ProductView User (List Product)
 
 
 init : () -> ( Model, Cmd Msg )
@@ -59,7 +60,9 @@ init _ =
 
 type Msg
     = GotUsers (Result Http.Error (List User))
+    | GotProducts (Result Http.Error (List Product))
     | ClickedUser User
+    | ClickedProduct User Product
     | GetUsers
 
 
@@ -74,11 +77,27 @@ update msg model =
                 Err _ ->
                     ( Failure, Cmd.none )
 
+        GotProducts result ->
+            case result of
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+                Ok products ->
+                    case model of
+                        LoadingProducts user ->
+                            ( ProductView user products, Cmd.none )
+
+                        _ ->
+                            ( Loading, getUsers )
+
         GetUsers ->
             ( Loading, getUsers )
 
         ClickedUser user ->
-            ( ProductView user, Cmd.none )
+            ( LoadingProducts user, getProducts )
+
+        ClickedProduct user product ->
+            ( Loading, getUsers )
 
 
 
@@ -113,9 +132,13 @@ view model =
             div []
                 [ h2 [] [ text "Loading" ] ]
 
-        ProductView user ->
+        LoadingProducts user ->
             div []
-                [ h2 [] [ text ("Clicked on " ++ user.name ++ "!") ]
+                [ h2 [] [ text "Loading Products" ] ]
+
+        ProductView user products ->
+            div []
+                [ Design.grid (List.map (\p -> productView user p) products)
                 , button [ onClick GetUsers ] [ text "Go Back" ]
                 ]
 
@@ -143,6 +166,22 @@ userView user =
         ]
 
 
+productView : User -> Product -> Html Msg
+productView user product =
+    div
+        [ onClick (ClickedProduct user product)
+        , style "margin" "10px"
+        , style "text-align" "center"
+        ]
+        [ img
+            [ src product.image
+            , style "height" "200px"
+            ]
+            []
+        , h3 [] [ text product.name ]
+        ]
+
+
 
 -- HTTP
 
@@ -151,7 +190,15 @@ getUsers : Cmd Msg
 getUsers =
     Http.get
         { url = "/static/users.json"
-        , expect = Http.expectJson GotUsers userListDecoder
+        , expect = Http.expectJson GotUsers (Json.Decode.list userDecoder)
+        }
+
+
+getProducts : Cmd Msg
+getProducts =
+    Http.get
+        { url = "/static/products.json"
+        , expect = Http.expectJson GotProducts (Json.Decode.list productDecoder)
         }
 
 
@@ -163,6 +210,10 @@ userDecoder =
         (field "avatar" string)
 
 
-userListDecoder : Decoder (List User)
-userListDecoder =
-    Json.Decode.list userDecoder
+productDecoder : Decoder Product
+productDecoder =
+    Json.Decode.map4 Product
+        (field "id" int)
+        (field "name" string)
+        (field "description" string)
+        (field "image" string)

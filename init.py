6bc5db5b43bd155d18cc329aa04fed7f7a4d07c8
Max Nagy <maxmunzel@me.com>
2021-09-12 13:57:51 +0200

    Updated init.py

--- init.py
+++ init.py
@@ -1,8 +1,17 @@
 import random
-from subprocess import run, PIPE
 import json
 import os
-import time
+import getpass
+import hashlib
+import sys
+
+try:
+    import jwt
+except ModuleNotFoundError:
+    print(
+        "Error! Could not create JWT Tokens for you - run `pip3 install pyjwt` and try again"
+    )
+    sys.exit(1)
 
 
 def generate_password():
@@ -10,9 +19,9 @@ def generate_password():
     return "".join(random.sample(alphabet, k=40))
 
 
-def get_postgrest_config(secret: str, db_rest_password: str) -> str:
+def get_postgrest_config(secret: str) -> str:
     conf = (
-        f"""db-uri = "postgres://rest:{db_rest_password}@localhost:5433/postgres"\n"""
+        f"""db-uri = "postgres://rest@db:5432/postgres"\n"""
         f"""db-schema = "strichliste"\n"""
         f"""db-anon-role = "web_anon"\n"""
         f"""jwt-secret = "{secret}"\n"""
@@ -22,44 +31,33 @@ def get_postgrest_config(secret: str, db_rest_password: str) -> str:
 
 
 if __name__ == "__main__":
-    db_pass = generate_password()  # password for the postgres superuser
     jwt_secret = generate_password()
-    db_rest_password = generate_password()  # used by postgREST
-    run(
-        f"docker run --name strichliste_postgres -p 5433:5432 -e POSTGRES_PASSWORD={db_pass} -d postgres",
-        shell=True,
-    )
-    time.sleep(5)
-    run(
-        f"sed 's/$PASSWORD/{db_rest_password}/g' postgres_schema.sql | psql -U postgres -h localhost -p 5433",
-        shell=True,
-        env=dict(os.environ, PGPASSWORD=db_pass, PGTIMEOUT="10"),
-    )
     with open("postgREST.conf", "w", os.O_CREAT) as f:
-        f.write(get_postgrest_config(jwt_secret, db_rest_password))
+        f.write(get_postgrest_config(jwt_secret))
 
-    tokens = []
-    try:
-        import jwt
+    # generate setup password
 
-        tokens = {
-            user: jwt.encode({"role": user}, jwt_secret, algorithm="HS256").decode()
-            for user in ["order_user", "xxxx_user"]
-        }
-    except ModuleNotFoundError:
-        print(
-            "Error! Could not create JWT Tokens for you - run `pip3 install pyjwt` and try again"
-        )
+    sha = hashlib.sha3_256()
+    sha.update(
+        getpass.getpass(
+            "Please Enter the Password used for setting up Tablets and backoffice access"
+        ).encode("utf8")
+    )
+    password_sha512 = sha.hexdigest()
+
+    tokens = {
+        user: jwt.encode({"role": user}, jwt_secret, algorithm="HS256").decode()
+        for user in ["order_user", "xxxx_user"]
+    }
     with open("secrets.json", "w", os.O_CREAT) as f:
         f.write(
             json.dumps(
                 {
                     "jwt_secret": jwt_secret,
-                    "db_pass": db_pass,
-                    "db_rest_password": db_rest_password,
+                    "password_sha512": password_sha512,
                     "tokens": tokens,
                 }
             )
         )
 
-    print(" Congig generation successful! Secrets stored in 'secrets.json'.")
+    print("Congig generation successful! Secrets stored in 'secrets.json'.")

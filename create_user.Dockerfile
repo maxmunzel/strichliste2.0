7255d7969613f82bca4a0f1d3934ea5dc1269be0
Max Nagy <maxmunzel@me.com>
2021-09-16 13:30:06 +0200

    Automatically Resize User Images

--- create_user.Dockerfile
+++ create_user.Dockerfile
@@ -4,4 +4,5 @@ RUN mkdir /go/src/create_user
 WORKDIR "/go/src/create_user"
 RUN ["go", "mod", "init"]
 RUN ["go", "get", "golang.org/x/crypto/sha3"]
-CMD [ "go",  "run",  "create_user.go" ]
+RUN ["go", "get", "golang.org/x/image/draw"]
+CMD ["go",  "run",  "create_user.go"]
--- create_user.go
+++ create_user.go
@@ -4,7 +4,12 @@ import (
 	"bytes"
 	"encoding/base32"
 	"encoding/json"
+	"errors"
 	"golang.org/x/crypto/sha3"
+	"golang.org/x/image/draw"
+	"image"
+	_ "image/jpeg"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"log"
@@ -32,6 +37,13 @@ func main() {
 		file_contents := bytes.NewBuffer(nil)
 		io.Copy(file_contents, file)
 
+		cropped, err := cropscalePng(file_contents, 200, 200)
+		if err != nil {
+			log.Printf("Error scaling image: %s\n", err)
+			http.Error(w, "Invalid Image", 400)
+			return
+		}
+
 		// prepend the files hash to make the filename (hence URL) non-enumeratable
 		hash := sha3.New256()
 		_, _ = hash.Write(file_contents.Bytes())
@@ -55,7 +67,7 @@ func main() {
 			return
 		}
 
-		err = ioutil.WriteFile(filename, file_contents.Bytes(), 0600)
+		err = ioutil.WriteFile(filename, cropped, 0600)
 		if err != nil {
 			log.Println("Cant write file")
 			log.Print(err)
@@ -65,4 +77,42 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(":8088", nil))
+}
+
+func cropscalePng(r io.Reader, target_x, target_y int) ([]byte, error) {
+	if target_x*target_y == 0 {
+		return nil, errors.New("target dimensions cannot be zero")
+	}
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	ratio := float32(img.Bounds().Dx()) / float32(img.Bounds().Dy())
+	ratio_target := float32(target_x) / float32(target_y)
+
+	var crop_x, crop_y int
+	if ratio < ratio_target {
+		crop_x = img.Bounds().Dx()
+		crop_y = int(float32(img.Bounds().Dx()) / ratio_target)
+	} else {
+		crop_y = img.Bounds().Dy()
+		crop_x = int(float32(img.Bounds().Dy()) * ratio_target)
+	}
+
+	rect := image.Rect(
+		img.Bounds().Min.X+(img.Bounds().Dx()/2)-crop_x/2,
+		img.Bounds().Min.Y+(img.Bounds().Dy()/2)-crop_y/2,
+		img.Bounds().Min.X+(img.Bounds().Dx()/2)+crop_x/2,
+		img.Bounds().Min.Y+(img.Bounds().Dy()/2)+crop_y/2,
+	)
+
+	scaled := image.NewNRGBA(image.Rect(0, 0, target_x, target_y))
+	draw.BiLinear.Scale(scaled, scaled.Rect, img, rect, draw.Over, nil)
+
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, scaled)
+
+	return buf.Bytes(), err
+
 }
